f33bd9f90fe60274889eb9fc12854d6742a36cb6
TimaPilgrim <timatok@gmail.com>
2019-11-26 14:05:59 +0300

    Optimize list back-end

--- Recorder/app/src/main/java/my/rockpilgrim/recorder/Model.kt
+++ Recorder/app/src/main/java/my/rockpilgrim/recorder/Model.kt
@@ -2,13 +2,18 @@ package my.rockpilgrim.recorder
 
 import android.net.Uri
 import android.os.Environment
+import android.util.Log
 import java.io.File
 import java.io.IOException
 
 class Model : Database {
 
     val packagePath = "/myrecorder/"
+    val trackName = "record_"
+    val trackFormat = ".mp3"
+
     private var output: String? = null
+    val TAG = "Model"
     private val dir: File =
         File(Environment.getExternalStorageDirectory().absolutePath + packagePath)
 
@@ -29,18 +34,20 @@ class Model : Database {
         if (dir.exists()) {
             val count = dir.listFiles().size
             output =
-                Environment.getExternalStorageDirectory().absolutePath + "${packagePath}recording_" + count + ".mp3"
+                Environment.getExternalStorageDirectory().absolutePath + "${packagePath}${trackName}" + count + trackFormat
         }
         return output
     }
 
     override fun getTrack(number: Int): Uri? {
-        return Uri.parse(Environment.getExternalStorageDirectory().absolutePath + "${packagePath}recording" + number + ".mp3")
+        return Uri.parse(Environment.getExternalStorageDirectory().absolutePath + "${packagePath}${trackName}" + number + trackFormat)
     }
 
     override fun getListCount(): Int {
         if (dir.exists()) {
+            Log.i(TAG,"getListCount dir: ${dir.listFiles().size}")
             return dir.listFiles().size
+//            return 1
         } else {
             return 0
         }
