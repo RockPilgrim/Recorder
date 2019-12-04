19b4b33d3317ac155d1d9683c52d98a05a4588dd
TimaPilgrim <timatok@gmail.com>
2019-12-04 13:31:41 +0300

    Popup menu in tracklist activity

--- Recorder/app/src/main/java/my/rockpilgrim/recorder/tracklist/TracklistHolder.kt
+++ Recorder/app/src/main/java/my/rockpilgrim/recorder/tracklist/TracklistHolder.kt
@@ -1,19 +1,49 @@
 package my.rockpilgrim.recorder.tracklist
 
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.widget.PopupMenu
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.track_item.view.*
 import my.rockpilgrim.recorder.R
-import my.rockpilgrim.recorder.record.ConnectToUI
 
-class TracklistHolder(itemView: View) : RecyclerView.ViewHolder(itemView), ConnectToList {
+class TracklistHolder(itemView: View) : RecyclerView.ViewHolder(itemView), ConnectToList{
 
     fun bind(position: Int, clickCallback: ClickCallback?) {
         itemView.trackNameTextView.text = "Track #${position + 1}"
         itemView.playButton.setOnClickListener {
             clickCallback?.onClick(position, this)
         }
+        itemView.trackSettingsButton.setOnClickListener {v ->
+            showMenu(v)
+        }
+    }
+
+    private fun showMenu(v: View) {
+        val popupMenu = PopupMenu(itemView.context, v)
+        popupMenu.inflate(R.menu.menu)
+        popupMenu.setOnMenuItemClickListener { itemMenu: MenuItem? ->
+            when (itemMenu?.itemId) {
+                R.id.deleteTrackMenuButton -> {
+                    deleteTrack()
+                    true
+                }
+                R.id.shareTrackMenuButton -> {
+                    true
+                }
+                else ->
+                    false
+            }
+
+        }
+        popupMenu.show()
+    }
+
+    private fun deleteTrack() {
+        makeToast("Track ${adapterPosition+1} Deleted")
     }
 
     override fun makeToast(line: String) {
