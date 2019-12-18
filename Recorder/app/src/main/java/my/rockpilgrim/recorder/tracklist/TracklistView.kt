0cba99111ca215e39b5f26716047f4cb1491f762
TimaPilgrim <timatok@gmail.com>
2019-12-18 19:08:25 +0300

    Add back button to ListActivity

--- Recorder/app/src/main/java/my/rockpilgrim/recorder/tracklist/TracklistView.kt
+++ Recorder/app/src/main/java/my/rockpilgrim/recorder/tracklist/TracklistView.kt
@@ -2,6 +2,7 @@ package my.rockpilgrim.recorder.tracklist
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import kotlinx.android.synthetic.main.list_layout.*
 import my.rockpilgrim.recorder.R
 
@@ -16,6 +17,26 @@ class TracklistView : AppCompatActivity() {
         setContentView(R.layout.list_layout)
 
         presenter = TracklistPresenter(this)
+
+        initToolBar()
+        initRecyclerList()
+    }
+
+    private fun initToolBar() {
+        val toolbar = listToolbar
+        setSupportActionBar(toolbar)
+
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setDisplayShowHomeEnabled(true)
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
+    private fun initRecyclerList() {
         myadapter = TracklistAdapter(presenter)
         myadapter.count = presenter.getCount()
         recyclerView.adapter = myadapter
